3473b876d1fa2209efa33adf7b62852560fa84cb
ferreira-fabio <fabioricardoferreira@hotmail.com>
2022-01-30 12:12:03 +0000

    Viewer Implement

--- Viewer.cs
+++ Viewer.cs
@@ -26,7 +26,30 @@ namespace EditorHtml
         public static void Replace(string text)
         {
             var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
-            System.Console.WriteLine(strong.IsMatch("strong"));//continuar daqui
+            System.Console.WriteLine(strong.IsMatch("strong"));
+            var words = text.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                
+                if (strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(
+                        words[i].Substring(
+                            words[i].IndexOf('>') + 1,
+                            ((words[i].LastIndexOf('<') - 1) - 
+                            words[i].IndexOf('>')) 
+                        )
+                    );
+                    Console.Write(" ");
+                }else{
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i]);
+                    Console.Write(" ");
+                }
+
+            }
 
         }
 
